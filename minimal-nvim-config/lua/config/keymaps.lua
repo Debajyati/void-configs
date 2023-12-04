c1ecd2295444e760a14c9a16a36979ac1944068d
Debajyati Dey <127122455+Debajyati@users.noreply.github.com>
2023-12-05 00:54:04 +0530

    Update keymaps.lua

--- minimal-nvim-config/lua/config/keymaps.lua
+++ minimal-nvim-config/lua/config/keymaps.lua
@@ -7,9 +7,27 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 -- telescope keymaps
 
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>gf', builtin.git_files, {})
+local telescope_builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', telescope_builtin.find_files, {})
+vim.keymap.set('n', '<leader>gf', telescope_builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ")});
+	telescope_builtin.grep_string({ search = vim.fn.input("Grep > ")});
 end)
+
+-- vim-fugitive keymaps
+vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+
+-- harpoon keymaps
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+
+-- undotree keymaps
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
