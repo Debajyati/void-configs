9b9a96d9994d76c05332106407faa35c211bd9f5
Debajyati Dey <127122455+Debajyati@users.noreply.github.com>
2023-12-05 00:38:42 +0530

    Update lazy.lua

--- minimal-nvim-config/lua/config/lazy.lua
+++ minimal-nvim-config/lua/config/lazy.lua
@@ -24,6 +24,17 @@ require("lazy").setup({
 	'nvim-telescope/telescope.nvim', tag = '0.1.4',
 	-- or                              , branch = '0.1.x',
 	dependencies = { 'nvim-lua/plenary.nvim' },
+	config = function()
+		require('telescope').setup({
+			defaults = {
+				layout_config = {
+					horizontal = {
+						preview_cutoff = 0,
+					},
+				},
+			},
+		})
+	end
     },
 
     {
