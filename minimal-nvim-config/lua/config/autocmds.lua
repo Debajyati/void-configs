c12634ac6949d70625c2944ac610306f95f894d2
Debajyati Dey <127122455+Debajyati@users.noreply.github.com>
2023-12-05 00:45:07 +0530

    Update autocmds.lua

--- minimal-nvim-config/lua/config/autocmds.lua
+++ minimal-nvim-config/lua/config/autocmds.lua
@@ -2,5 +2,15 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 
+-- colorscheme command
+-- vim.cmd [[colorscheme habamax]]
+function ColorMyCanvas(color)
+	color = color or habamax
+	vim.cmd.colorscheme(color)
 
-vim.cmd [[colorscheme habamax]]
+--	vim.api.nvim_set_hl(0, "Normal", { bg="none"})
+--	vim.api.nvim_set_hl(0, "NormalFloat", { bg="none" })
+
+end
+
+ColorMyCanvas()
