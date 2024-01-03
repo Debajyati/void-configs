e542b85c3657722a7410c1fe70333b92d7b57fbc
Debajyati Dey <127122455+Debajyati@users.noreply.github.com>
2024-01-03 21:02:49 +0530

    Update bash-aliases.sh

--- bash-aliases.sh
+++ bash-aliases.sh
@@ -3,23 +3,23 @@ alias egrep='egrep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias grep='grep --color=auto'
 # Listing Aliases
-alias etree='exa --tree --icons'
-alias ls='ls --color=auto'
+alias etree='exa --tree --icons' # exa needs to be preinstalled
+alias ls='ls --color=auto' 
 alias la='ls -A'
-alias laa='exa -a'
+alias laa='exa -a' # exa needs to be preinstalled
 alias ll='ls -alF'
 alias l='ls -CF'
 # Directory navigation with fuzzy finder
-alias cdf='cd $(find * -type d | fzf)'
-alias cdpf='cd $(fd -t d | fzf)'
-alias sad='cd ~ && cd $(fd -t d | fzf)'
+alias cdf='cd $(find * -type d | fzf)' # fzf needs to be preinstalled
+alias cdpf='cd $(fd -t d | fzf)' # fzf and fd both needs to be preinstalled
+alias sad='cd ~ && cd $(fd -t d | fzf)' # fzf and fd both needs to be preinstalled
 # file navigation with search & preview
-alias pf='fzf --preview='\''bat --color=always {}'\'' --bind shift-up:preview-page-up,shift-down:preview-page-down'
-alias plf='fzf --preview='\''less {}'\'' --bind shift-up:preview-page-up,shift-down:preview-page-down'
+alias pf="fzf --preview='bat --color=always {}' --bind shift-up:preview-page-up,shift-down:preview-page-down" # fzf & bat both needs to be preinstalled
+alias plf="fzf --preview='less {}' --bind shift-up:preview-page-up,shift-down:preview-page-down" # fzf and less both needs to be preinstalled
 # for convenience
 alias cls='clear'
 alias python='python3'
 alias nf='neofetch'
 alias rm='trash'
 # git aliases
-alias gba='git log --oneline --decorate --all --graph'
+alias gba='git log --oneline --decorate --all --graph' # git needs to be preinstalled
